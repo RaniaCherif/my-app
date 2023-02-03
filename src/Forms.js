d5c0407c1c672a978466929724be0d4d4e46c0f0
rania <contactraniacherif@gmail.com>
2023-02-03 07:49:51 +0100

    install boostrap react

---
+++ src/Forms.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./StyleForms.css";
+
+function Forms() {
+  return (
+
+  );
+}
+
+export default Forms;
