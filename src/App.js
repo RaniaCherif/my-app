4771862fb3fe15f4e96106aaf8913c0069932498
rania <contactraniacherif@gmail.com>
2023-02-03 09:56:12 +0100

    create component FormSpiner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./App.css";
 import Forms from "./Forms";
+import FormSpiner from "./FormSpiner";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Forms/>
+    <>
+      <Routes>
+        <Route
+          path="/Forms"
+          element={
+            <React.Fragment>
+              <Forms />
+            </React.Fragment>
+          }
+        />
+        <Route
+          path="/FormSpiner"
+          element={
+            <React.Fragment>
+              <FormSpiner />
+            </React.Fragment>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
--- src/Forms.js
+++ src/Forms.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./StyleForms.css";
 
-function Forms() {
+const Forms = () => {
   return (
     <div class="login-box">
       <h2>Login</h2>
@@ -17,10 +15,17 @@ function Forms() {
           <input type="password" name="" required=""></input>
           <label>Password</label>
         </div>
-        <a href="#">Submit</a>
+
+        <a href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit
+        </a>
       </form>
     </div>
   );
-}
+};
 
 export default Forms;
