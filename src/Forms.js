1a3a3d5bfb33373d3cab36a3718828ae3fca8eda
rania <contactraniacherif@gmail.com>
2023-02-03 07:50:25 +0100

    creat Forms component

--- src/Forms.js
+++ src/Forms.js
@@ -6,7 +6,20 @@ import "./StyleForms.css";
 
 function Forms() {
   return (
-
+    <div class="login-box">
+      <h2>Login</h2>
+      <form>
+        <div class="user-box">
+          <input type="text" name="" required=""></input>
+          <label>Username</label>
+        </div>
+        <div class="user-box">
+          <input type="password" name="" required=""></input>
+          <label>Password</label>
+        </div>
+        <a href="#">Submit</a>
+      </form>
+    </div>
   );
 }
 
